d309d61f699750f0630cf1bbb23bd41196839227
“wangyantao” <wangyantao@erwan365.com>
2021-10-04 15:47:54 +0800

    自定义starter

--- wytlab-erwan/src/main/java/leetcode/Ltest.java
+++ wytlab-erwan/src/main/java/leetcode/Ltest.java
@@ -3,6 +3,8 @@ package leetcode;
 public class Ltest {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.removeDuplicates(new int[]{0,0,1,13});
+//        solution.removeDuplicates(new int[]{0,0,1,13});
+//        System.out.println(solution.plusOne(new int[]{9,9}).toString());
+        System.out.println(solution.evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
     }
 }
