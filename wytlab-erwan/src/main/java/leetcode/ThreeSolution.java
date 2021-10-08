ea2093b32aed32f62cea8a6316abbb8e88824681
wangyantao1 <wangyantao1@jd.com>
2021-10-08 23:05:38 +0800

    枚举转map

--- wytlab-erwan/src/main/java/leetcode/ThreeSolution.java
+++ wytlab-erwan/src/main/java/leetcode/ThreeSolution.java
@@ -1,6 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ThreeSolution {
@@ -18,8 +20,59 @@ public class ThreeSolution {
     }
     //模拟行走机器人
     public int robotSim(int[] commands, int[][] obstacles) {
+       return  0;
 
     }
 
-    private
+    //https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/submissions/
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<>();
+        wordsMap = new HashMap<>();
+        int total = 0;
+        for (String word : words) {
+            total += word.length();
+            wordsMap.put(word, wordsMap.get(word) == null ? 1 : wordsMap.get(word) + 1);
+        }
+
+        for (int i = 0; i + total <= s.length(); i++) {
+            if(validate(s.substring(i, i + total), words)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean validate(String s, String[] words) {
+        int len = words[0].length();
+        Map<String, Integer> splitMap = new HashMap<>();
+        for (int i = 0; i < s.length() ; i += len) {
+            String str = s.substring(i, i + len);
+            splitMap.put(str, splitMap.get(str) == null ? 1 : splitMap.get(str) + 1);
+        }
+        return equalsMap(wordsMap, splitMap);
+    }
+
+    private boolean equalsMap(Map<String, Integer> src, Map<String, Integer> des) {
+        for(Map.Entry<String, Integer> entry : src.entrySet()) {
+            if(!des.containsKey(entry.getKey()) || !entry.getValue().equals(des.get(entry.getKey()))) {
+                return false;
+            }
+        }
+        for(Map.Entry<String, Integer> entry : des.entrySet()) {
+            if(!src.containsKey(entry.getKey()) || !entry.getValue().equals(src.get(entry.getKey()))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private  Map<String, Integer> wordsMap;
+
+
+    public static void main(String[] args) {
+        System.out.println("aaaa".substring(0,4));
+    }
+
+
 }
