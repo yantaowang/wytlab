c399a72064a57fc2e1efda67a9d9bf1179077a32
wangyantao <wangyantao@erwan.com>
2021-10-10 15:03:09 +0800

    自定义starter

--- wytlab-erwan/src/main/java/leetcode/ThreeSolution.java
+++ wytlab-erwan/src/main/java/leetcode/ThreeSolution.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ThreeSolution {
     //两数之和
@@ -18,8 +17,58 @@ public class ThreeSolution {
     }
     //模拟行走机器人
     public int robotSim(int[] commands, int[][] obstacles) {
+       Set<String> set = new HashSet<>();
+        for (int i = 0; i < obstacles.length; i++) {
+            set.add(getSetVal(obstacles[i]));
+        }
+        int dir = 0, ans = 0;
+        int x = 0, y = 0;
+        //方向数组
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
 
+        for(int command : commands) {
+            if(command == -1) {
+                // TODO: 2021/10/7
+                dir = (dir + 1) % 4;
+            } else if (command == -2) {
+                // TODO: 2021/10/7  
+                dir = (dir + 3) % 4;
+            } else {
+                for (int i = 0; i < command; i++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+                    if(set.contains(getSetVal(new int[]{nx, ny}))) {
+                        break;
+                    }
+                    x = nx;
+                    y = ny;
+                    ans = Math.max(ans, x * x + y * y);
+                }
+            }
+        }
+        return ans;
     }
 
-    private
+    private String getSetVal(int[] obs) {
+        return obs[0] + "-" + obs[1];
+    }
+
+    //https://leetcode-cn.com/problems/group-anagrams/  49. 字母异位词分组
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public int numberOfSubarrays(int[] nums, int k) {
+
+    }
 }
