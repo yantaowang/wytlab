bca6e7660e5287ed6b9cc3337a1eb8460dd40a64
wangyantao <wangyantao@erwan.com>
2021-10-23 10:07:22 +0800

    leetcode

--- wytlab-erwan/src/main/java/leetcode/FourSolution.java
+++ wytlab-erwan/src/main/java/leetcode/FourSolution.java
@@ -1,9 +1,30 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //递归
 public class FourSolution {
 
-
+    //https://leetcode-cn.com/problems/subsets/
+    public List<List<Integer>> subsets(int[] nums) {
+        n = nums.length;
+        recur(nums, 0);
+        return ans;
+    }
+    private void recur(int[] nums, int i) {
+        if(i == n) {
+            ans.add(new ArrayList<Integer>(chosen));
+            return;
+        }
+        recur(nums,i+1);
+        chosen.add(nums[i]);
+        recur(nums, i+1);
+        chosen.remove(chosen.size()-1);
+    }
+    private List<List<Integer>> ans = new ArrayList<>();
+    private List<Integer> chosen = new ArrayList<>();
+    private int n;
 
     //递归源码
 //    void recursion(int level, int param) {
